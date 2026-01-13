e57ef0199fe3cc36ccc1829b1d6f27a660503544
Diego Pires <71283254+Didigos@users.noreply.github.com>
2026-01-13 09:09:26 -0300

    ajuste no layout do formulário de login e correção de rótulos

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -13,25 +13,25 @@ const Login = () => {
         <main className=" w-screen h-screen flex items-center justify-center">
             <section className="sm:w-[85%] sm:h-[90%]">
                 <div className="text-center">
-                    <h2 className="text-[3.2rem]">Acbr Celulares</h2>
+                    <h2 className="text-[2.8rem]">Acbr Celulares</h2>
                     <span>Ordens de serviço V 0.1</span>
                 </div>
 
                 <form className="flex flex-col mt-[50%] items-center gap-4">
                     <div className="flex flex-col p-2">
                         <label
-                            className="text-[1.2rem] font-bold"
-                            htmlFor="username">Username</label>
+                            className="text-[1.2rem] font-bold ml-10"
+                            htmlFor="username">Funcionário</label>
                         <input
-                            className="border border-black w-90 h-20 rounded-full text-[2.2rem] p-2 text-center outline-none"
+                            className="border border-black w-70 h-20 rounded-full text-[2.2rem] p-2 text-center outline-none"
                             type="text" />
                     </div>
                     <div className="flex flex-col p-2">
                         <label
-                            className="text-[1.2rem] font-bold"
-                            htmlFor="username">password</label>
+                            className="text-[1.2rem] font-bold ml-10"
+                            htmlFor="password">Senha de acesso</label>
                         <input
-                            className="border border-black w-90 h-20 rounded-full text-[2.2rem]  p-2 text-center outline-none"
+                            className="border border-black w-70 h-20 rounded-full text-[2.2rem] p-2 text-center outline-none"
                             type="password" />
                     </div>
                     <button
