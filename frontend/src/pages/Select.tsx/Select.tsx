421a3a3da79ae6e4968269c59f540cb3ce7f6fd1
Diego Pires <71283254+Didigos@users.noreply.github.com>
2026-01-13 09:11:56 -0300

    ajuste no tamanho do título e dos botões no formulário de seleção

--- frontend/src/pages/Select.tsx/Select.tsx
+++ frontend/src/pages/Select.tsx/Select.tsx
@@ -15,18 +15,18 @@ const Select = () => {
         <main className="w-screen h-screen flex items-center justify-center">
             <section className="sm:w-[85%] sm:h-[90%]">
                 <div className="text-center">
-                    <h2 className="text-[3.2rem]">Acbr Celulares</h2>
+                    <h2 className="text-[2.8rem]">Acbr Celulares</h2>
                     <span>Ordens de serviço V 0.1</span>
                 </div>
 
-                <form className="flex flex-col mt-[50%] items-center gap-20">
+                <form className="flex flex-col mt-[30%] items-center gap-20">
                     <button
                         onClick={handleReceiveDevice}
-                        className="border p-2 rounded-2xl w-100 h-40 bg-cyan-500 text-white text-[1.2rem] font-bold"
+                        className="border p-2 rounded-2xl w-80 h-40 bg-cyan-500 text-white text-[1.2rem] font-bold"
                     >RECEBER APARELHO</button>
                     <button
                         onClick={handleDeliverDevice}
-                        className="border p-2 rounded-2xl w-100 h-40 bg-green-500 text-white text-[1.2rem] font-bold"
+                        className="border p-2 rounded-2xl w-80 h-40 bg-green-500 text-white text-[1.2rem] font-bold"
                     >ENTREGAR APARELHO</button>
                 </form>
 
