ef4f7ba99125a6225959ee15eec0d5caeb411294
Diego Pires <71283254+Didigos@users.noreply.github.com>
2026-01-16 21:52:18 -0300

    refatora conexão com o MongoDB para incluir tratamento de eventos e timeout

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,10 +1,20 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
+mongoose.set("bufferCommands", false);
 
 export async function connectDB() {
-  
-  const uri = process.env.MONGODB_URI
-  if (!uri) throw new Error('MONGODB_URI não definido no .env')
+  const uri = process.env.MONGODB_URI;
+  if (!uri) throw new Error("MONGODB_URI não definido");
 
-  await mongoose.connect(uri)
-  console.log('✅ MongoDB conectado')
+  mongoose.connection.on("connected", () => {
+    console.log("✅ MongoDB conectado");
+    console.log("DB:", mongoose.connection.name);
+  });
+
+  mongoose.connection.on("error", (err) => {
+    console.log("❌ Erro MongoDB:", err);
+  });
+
+  await mongoose.connect(uri, {
+    serverSelectionTimeoutMS: 15000,
+  });
 }
