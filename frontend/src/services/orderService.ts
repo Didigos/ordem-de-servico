28ac12beb758dbcebcf8c3891eade95688f14df6
Diego Pires <71283254+Didigos@users.noreply.github.com>
2026-01-16 21:16:10 -0300

    refatora URL da API para usar variável de ambiente

--- frontend/src/services/orderService.ts
+++ frontend/src/services/orderService.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3333';
+
 type OrderData = {
     customerId: string;
     brand: string;
@@ -12,7 +14,7 @@ type OrderData = {
 
 export async function sendOrderData(orderData: OrderData) {
     try {
-    const response = await axios.post('http://localhost:3333/orders', orderData)
+    const response = await axios.post(`${API_URL}/orders`, orderData)
     console.log('resposta do servidor: ', response.data);
     return response
     }
@@ -24,7 +26,7 @@ export async function sendOrderData(orderData: OrderData) {
 
 export async function getOrders() {
     try {
-        const response = await axios.get('http://localhost:3333/orders');
+        const response = await axios.get(`${API_URL}/orders`);
         return response.data;
     }
     catch (error) {
